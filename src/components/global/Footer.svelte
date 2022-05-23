966171acf14cc11263a7bccf001304a47be74142
Muhammad-Rubel <rubel.contact@gmail.com>
2022-05-23 20:43:53 +0600

    Footer Completed.

--- src/components/global/Footer.svelte
+++ src/components/global/Footer.svelte
@@ -1,1 +1,5 @@
-<footer>Footer</footer>
+<footer class="px-4 py-6">
+	<p class="text-deep-blue font-bold text-center">
+		Copyright &copy; <span class="text-light-green">Muhammad Rubel</span> | All Rights Reserved
+	</p>
+</footer>
